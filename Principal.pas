144107dbfabb43eca1b81e7f760a445f070365de
Francisco Sáez <fjsaez@gmail.com>
2021-07-25 00:29:02 -0400

    Eliminado código sobrante. Otros ajustes visuales

--- Principal.pas
+++ Principal.pas
@@ -9,17 +9,14 @@ uses
   System.Sensors.Components, FMX.Objects, FMX.Platform.Android, UTM_WGS84;
 
 type
-  TForm1 = class(TForm)
-    Layout1: TLayout;
+  TFPrinc = class(TForm)
+    LayFlecha: TLayout;
     CrcKingOTN: TCircle;
     LayBot: TLayout;
     SpeedButton1: TSpeedButton;
     StyleBook: TStyleBook;
     VertScrollBox1: TVertScrollBox;
     LayMap: TLayout;
-    LayLED: TLayout;
-    SwitchGPS: TSwitch;
-    LActivar: TLabel;
     Layout4: TLayout;
     LayLat: TLayout;
     Label2: TLabel;
@@ -39,11 +36,44 @@ type
     Label7: TLabel;
     LctSensor: TLocationSensor;
     SpeedButton2: TSpeedButton;
+    LayLED: TLayout;
+    SwitchGPS: TSwitch;
+    LActivar: TLabel;
+    Rectangle1: TRectangle;
+    RectActivar: TRectangle;
+    Rectangle3: TRectangle;
+    Rectangle4: TRectangle;
+    Layout1: TLayout;
+    Layout2: TLayout;
+    LRumbo: TLabel;
+    Label1: TLabel;
+    Label8: TLabel;
+    LAzimut: TLabel;
+    PnlAcerca: TPanel;
+    LayPrinc: TLayout;
+    Layout3: TLayout;
+    Layout5: TLayout;
+    Layout6: TLayout;
+    Layout7: TLayout;
+    Layout8: TLayout;
+    Layout9: TLayout;
+    Layout10: TLayout;
+    Layout11: TLayout;
+    Image1: TImage;
+    Label9: TLabel;
+    Label10: TLabel;
+    Label11: TLabel;
+    Label13: TLabel;
+    BAceptar: TButton;
+    Rectangle2: TRectangle;
     procedure LctSensorLocationChanged(Sender: TObject; const OldLocation,
       NewLocation: TLocationCoord2D);
     procedure SwitchGPSSwitch(Sender: TObject);
     procedure SpeedButton1Click(Sender: TObject);
     procedure SpeedButton2Click(Sender: TObject);
+    procedure LctSensorHeadingChanged(Sender: TObject;
+      const AHeading: THeading);
+    procedure BAceptarClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -51,13 +81,49 @@ type
   end;
 
 var
-  Form1: TForm1;
+  FPrinc: TFPrinc;
 
 implementation
 
 {$R *.fmx}
 
-procedure TForm1.LctSensorLocationChanged(Sender: TObject; const OldLocation,
+function Orientacion(Grados: double): string;
+begin
+  case Round(Grados) of
+    0..10,350..360: Result:='N';  //norte
+    11..34: Result:='N - NE';     //norte-noreste
+    35..54: Result:='NE';         //noreste
+    55..79: Result:='E - NE';     //este-noreste
+    80..100: Result:='E';         //este
+    101..124: Result:='E - SE';   //este-sureste
+    125..144: Result:='SE';       //sureste
+    145..169: Result:='S - SE';   //sur-sureste
+    170..190: Result:='S';        //sur
+    191..214: Result:='S - SW';   //sur-suroeste
+    215..234: Result:='SW';       //suroeste
+    235..259: Result:='W - SW';   //oeste-suroeste
+    260..280: Result:='W';        //oeste
+    281..304: Result:='W - NW';   //oeste-noroeste
+    305..324: Result:='NW';       //noroeste
+    325..349: Result:='N - NW';   //norte-noroeste
+  end;
+end;
+
+procedure TFPrinc.BAceptarClick(Sender: TObject);
+begin
+  PnlAcerca.Visible:=false;
+  LayPrinc.Visible:=true;
+end;
+
+procedure TFPrinc.LctSensorHeadingChanged(Sender: TObject;
+  const AHeading: THeading);
+begin
+  CrcKingOTN.RotationAngle:=AHeading.Azimuth;
+  LAzimut.Text:=FormatFloat('0.##',AHeading.Azimuth)+'º';
+  LRumbo.Text:=Orientacion(AHeading.Azimuth);
+end;
+
+procedure TFPrinc.LctSensorLocationChanged(Sender: TObject; const OldLocation,
   NewLocation: TLocationCoord2D);
 var
   LDecSeparator: String;
@@ -76,35 +142,41 @@ begin
   LNorte.Text:=FormatFloat('#0.00',UTM.Y);
 end;
 
-procedure TForm1.SpeedButton1Click(Sender: TObject);
+procedure TFPrinc.SpeedButton1Click(Sender: TObject);
 begin
   //MainActivity.finish;
   Application.Terminate;
 end;
 
-procedure TForm1.SpeedButton2Click(Sender: TObject);
+procedure TFPrinc.SpeedButton2Click(Sender: TObject);
 begin
-  ShowMessage('Simple GPS'+#13#10+'v1.0'+#13#10#13#10+
+  {ShowMessage('Simple GPS'+#13#10+'v1.0'+#13#10#13#10+
     'Autor: Francisco J. Sáez S.'+#13#10#13#10+
-    'Calabozo, 03 de julio de 2021');
+    'Calabozo, 03 de julio de 2021');}
+  LayPrinc.Visible:=false;
+  PnlAcerca.Visible:=true;
 end;
 
-procedure TForm1.SwitchGPSSwitch(Sender: TObject);
+procedure TFPrinc.SwitchGPSSwitch(Sender: TObject);
 begin
   LctSensor.Active:=SwitchGPS.IsChecked;
   if SwitchGPS.IsChecked then
   begin
     LActivar.Text:='Desactivar GPS';
-    CrcKingOTN.Stroke.Color:=$FF7FFF00   //chartreuse
+    RectActivar.Fill.Color:=$FF7FFF00;
+    CrcKingOTN.Stroke.Color:=$FF7FFF00;   //chartreuse
   end
   else
   begin
     LActivar.Text:='Activar GPS';
+    RectActivar.Fill.Color:=$FFFF0000;
     CrcKingOTN.Stroke.Color:=$FFFF0000;  //rojo
     LLat.Text:='--.-----';
     LLon.Text:='--.-----';
     LEste.Text:='--';
     LNorte.Text:='--';
+    LAzimut.Text:='--';
+    LRumbo.Text:='--';
   end;
 end;
 
--- SimpleGPS.dpr
+++ SimpleGPS.dpr
@@ -2,13 +2,18 @@ program SimpleGPS;
 
 uses
   System.StartUpCopy,
+  Androidapi.JNI.App,
+  Androidapi.JNI.GraphicsContentViewText,
+  Androidapi.Helpers,
   FMX.Forms,
-  Principal in 'Principal.pas' {Form1};
+  Principal in 'Principal.pas' {FPrinc};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm1, Form1);
+  SharedActivity.getWindow.addFlags(TJWindowManager_LayoutParams.JavaClass.FLAG_KEEP_SCREEN_ON);
+  Application.FormFactor.Orientations := [TFormOrientation.Portrait, TFormOrientation.InvertedPortrait, TFormOrientation.Landscape, TFormOrientation.InvertedLandscape];
+  Application.CreateForm(TFPrinc, FPrinc);
   Application.Run;
 end.
